374167f82a5d34cdd2f73e087e973ddfed3c77de
josephpbermingham <josephpbermingham@gmail.com>
2017-11-06 10:42:55 -0500

    This is a functioning version of sell and invoice. just need to figure out how to return it

--- Employee.java
+++ Employee.java
@@ -1,22 +1,29 @@
 package Project3GitDoneRight;
 public class Employee {
-    private String firstName;
-    private String lastName;
+    private String userType;
+    private String phoneNumber;
     private String username;
     private String password;
     private String email;
 
 
-    public String getFirstName() {
-        return firstName;
+
+
+    public String getUserType() {
+        return userType;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public String getUsername() {
         return username;
+    }
+
+
+    public void setPassword(String pass){
+        this.password = pass;
     }
 
     public String getPassword(boolean a) {
@@ -31,19 +38,25 @@ public class Employee {
     }
 
 
-    public Employee(String fName, String lName, String uName, String Pword, String Email) {
-        this.firstName = fName;
-        this.lastName = lName;
+    public Employee(String uType, String pNum,  String Email, String uName, String Pword) {
+        this.userType = uType;
+        this.phoneNumber = pNum;
         this.username = uName;
         this.password = Pword;
         this.email = Email;
     }
 
     public Employee() {
-        this.firstName = "John";
-        this.lastName = "doe";
-        this.username = "SysAdmin";
-        this.password = "A";
+        this.userType = "SysAdmin";
+        this.phoneNumber = "4341234444";
         this.email = "J@J.com";
+        this.username = "JohnSmith";
+        this.password = "A";
+    }
+
+    @Override
+    public String toString(){
+        return userType + "," + phoneNumber+ "," + email +","+ username+"," + password;
     }
 }
+
---
+++ SysAdmin.java
@@ -0,0 +1,144 @@
+package Project3GitDoneRight;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author zachcaton
+ */
+public class SysAdmin extends Employee{
+
+
+    private ArrayList<Employee> users = new ArrayList<>();
+    private String fileName = "users.txt";
+    private File f;//todo discuss that I do not use an employees file, i create my own
+
+    /**
+     * Adds user to user database (text file)
+     * @param userType what type of user this user is
+     * @param phoneNumber the pnone nubmer of this user
+     * @param email this users email
+     * @param userName this users username
+     * @param password this users unencrypted password
+     * @throws FileNotFoundException
+     */
+    public void addUser(String userType, String phoneNumber, String email, String userName, String password) throws FileNotFoundException{
+
+        //creating an employee from text fields
+        String [] user = new String[5];
+        user[0] = userType;
+        user[1] = phoneNumber;
+        user[2] = email;
+        user[3] = userName;
+        user[4] = password;
+        Employee em = new Employee(user[0], user[1], user[2], user[3], user[4]);
+
+        //checks to see if users is empty, if it is then itll read in users fro file
+        if (users.isEmpty() == true){
+
+            Scanner inVF = new Scanner(f);
+            //adds users back into program
+            while(inVF.hasNext()){
+                String line = inVF.nextLine();
+                String [] pA = line.split(",");
+                Employee ba = new Employee(pA[0], pA[1], pA[2], pA[3], pA[4]);
+                users.add(ba);
+            }
+
+        }else{
+            //compares users to the name of the person who is trying to be added
+            for (int i=0; i < users.size(); i++){
+                //if new username does not match that of one in the file it'll be added
+                if (users.get(i).getUsername().compareTo(userName) != 0){
+                    users.add(em);
+                }else{
+                    System.out.println("User already exsists");
+                }
+            }
+        }
+        //after everything is complete it writes the users back into the file
+        f = new File(fileName);
+        try (PrintWriter p = new PrintWriter(f)) {
+            for(Employee usr: users){
+                p.println(usr.toString());
+            }
+        }
+
+    }
+
+    /**
+     * Deletes user by his/her username
+     * @param username
+     * @throws FileNotFoundException
+     */
+    public void deleteUser(String username) throws FileNotFoundException{
+        //read from user.txt
+        //then compare names and delete the correct one
+        f = new File(fileName);
+        if(users.isEmpty() == true){
+
+            Scanner inVF = new Scanner(f);
+
+            while(inVF.hasNext()){
+                String line = inVF.nextLine();
+                String [] pA = line.split(",");
+                Employee ba = new Employee(pA[0], pA[1], pA[2], pA[3], pA[4]);
+                users.add(ba);
+            }
+            for(int i=0; i< users.size(); i++){
+                if(users.get(i).getUsername().compareTo(username) == 0){
+                    users.remove(i);
+                }
+            }
+            //File f = new File(fileName);
+            try (PrintWriter p = new PrintWriter(f)) {
+                for(Employee usr: users){
+                    p.println(usr.toString());
+                }
+            }
+        }else{
+            for(int i=0; i< users.size(); i++){
+                if(users.get(i).getUsername().compareTo(username) == 0){
+                    users.remove(i);
+                }
+            }
+            //File f = new File(fileName);
+            try (PrintWriter p = new PrintWriter(f)) {
+                for(Employee usr: users){
+                    p.println(usr.toString());
+                }
+            }
+        }
+
+    }
+    /**
+     * Resets password by user name
+     * @param name
+     * @param newPass
+     * @throws FileNotFoundException
+     */
+    public void resetPassword(String name, String newPass) throws FileNotFoundException{
+
+        for(int i=0; i< users.size(); i++){
+            if(users.get(i).getUsername().compareTo(name) == 0){
+                //change pass to new pass
+                users.get(i).setPassword(newPass);
+
+            }else{
+                System.out.println("Something went wrong with reseting password");
+            }
+        }
+
+        //File f = new File(fileName);
+        try (PrintWriter p = new PrintWriter(f)) {
+            for(Employee usr: users){
+                p.println(usr.toString());
+            }
+        }
+
+    }
+
+}
