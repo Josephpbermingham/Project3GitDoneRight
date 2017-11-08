85bb99c91dc16053c50c0bc1272221de060fa09f
zachcaton <zachcaton@10.35.116.62>
2017-11-08 06:52:34 -0500

    tester

---
+++ project3_groupCaton/src/Project3GitDoneRight/Test/SalesAsscTester.java
@@ -0,0 +1,34 @@
+package Project3GitDoneRight.Test;
+
+import Project3GitDoneRight.SalesAssociate;
+
+import java.util.ArrayList;
+
+public class SalesAsscTester{
+    private static SalesAssociate bob;
+
+    public static void main(String[] args) {
+
+        bob = new SalesAssociate("Bob", "Brinkle", "bbrinkle", "t", "email", "123-343-3243");
+        sellstuff();
+        //addstuff();
+        invoicing();
+    }
+
+    private static void sellstuff() {
+        bob.Sell("", 1, 10);
+        bob.Sell("AAA_FIRST", -1, 1);
+        bob.Sell("10spFrontDerailuer",-1,2);
+        bob.Sell("spdPedals",-1,3);
+        bob.Sell("",1234567910,4);
+    }
+
+    private static void addstuff() {
+        bob.LoadFile("initialInv.txt");
+    }
+    private static void invoicing(){
+        ArrayList<Object> a = bob.closeinvoice("marko");
+
+        System.out.println(a.get(0).toString());
+    }
+}
