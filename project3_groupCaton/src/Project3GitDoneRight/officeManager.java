cc97f00cb3c4f9463cb8a1cb62b442b4b9ffb400
zachcaton <zachcaton@10.35.116.62>
2017-11-08 06:12:34 -0500

    trying to set it all up still

--- project3_groupCaton/src/Project3GitDoneRight/officeManager.java
+++
@@ -1,6 +0,0 @@
-package Project3GitDoneRight;
-public class officeManager extends Employee {
-    public officeManager(String a, String b, String c, String d, String e) {
-        super(a, b, c, d, e);
-    }
-}
